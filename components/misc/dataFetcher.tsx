1116428e009f04b73154a43c9dcde06af25806ae
Adetimmy <ogunleademola@gmail.com>
2025-02-11 15:13:06 +0100

    set-up data fetcher for hook/component for any data fetching

---
+++ components/misc/dataFetcher.tsx
@@ -0,0 +1,23 @@
+import useToastNofication from "@/lib/handleToast";
+import { useFetch } from "@/lib/services";
+import { ChildrenProps } from "@/lib/types";
+import React from "react";
+
+interface DataFetcherProps {
+  url: string;
+  children: (data: unknown | any) => React.ReactNode;
+}
+
+export const DataFetcher: React.FC<DataFetcherProps> = ({ url, children }) => {
+  const { data, isLoading } = useFetch({ url });
+  console.log(data);
+  if (isLoading) {
+    return <div className="justify-center flex items-center h-full"><p>loading...</p></div>;
+  }
+  if (data?.error) {
+    useToastNofication(data.error);
+  }
+  if (data) {
+    return <>{children(data?.data)}</>;
+  }
+};
