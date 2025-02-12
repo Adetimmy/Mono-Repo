9933e36e2d4bea38dd34b72170a9e0910e287a96
Adetimmy <ogunleademola@gmail.com>
2025-02-12 08:02:35 +0100

    applying a full screen size

--- app/page.tsx
+++ app/page.tsx
@@ -22,99 +22,117 @@ import { useState } from "react";
 export default function Home() {
   const [id, setId] = useState(1);
   return (
-    <DataFetcher url="/posts">
-      {(data) => {
-        return (
-          <ResizablePanelGroup
-            direction="horizontal"
-
-            className="max-w-md rounded-lg border md:min-w-7xl m-auto mt-5 min-h-auto"
-          >
-            <ResizablePanel defaultSize={50} className="h-[650px]">
-              <ScrollArea className="h-full">
-                <div className="flex h-auto flex-col  py-6">
-                  {data?.map((item: any) => (
-                    <div
-                      key={item.id}
-                      className={cn(
-                        " p-2 line-clamp-1 cursor-pointer",
-                        item.id === id ? "bg-amber-700" : "hover:bg-red-500"
-                      )}
-                      onClick={() => setId(item.id)}
-                    >
-                      <span className="font-semibold">{item.title}</span>
-                    </div>
-                  ))}
-                </div>
-              </ScrollArea>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel defaultSize={50}>
-              <ResizablePanelGroup direction="vertical">
-                <ResizablePanel defaultSize={25}>
-                  <ScrollArea className="">
-                    <DataFetcher url={`/photos/${id}`}>
-                      {(data) => {
-                        return (
-                          <Image
-                            src={data?.url}
-                            loader={() => {
-                              return data?.thumbnailUrl;
-                            }}
-                            alt={data?.title}
-                            width={500}
-                            height={500}
-                          />
-                        );
-                      }}
-                    </DataFetcher>
-                  </ScrollArea>
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel defaultSize={75}>
-                  <ScrollArea className="h-full px-4">
-                    <DataFetcher url={`/comments?postId=${id}`}>
-                      {(data) => {
-                
-                        return (
-                          <>
-                            {data?.map((item:{
-                              name:string,
-                              email:string,
-                              body:string
-                            }, index:number) => {
-                              return (
-                                <Card className={cn('drop-shadow-sm',  index===0 && index === data.length - 1 ? '':'my-5')}>
-                                  <CardHeader className="flex flex-row items-start gap-3">
-                                    <Avatar className="size-12">
-                                      <AvatarFallback>
-                                        <User2Icon />
-                                      </AvatarFallback>
-                                    </Avatar>
-                                    <div className="flex flex-col">
-                                      <CardTitle>{capitalizeFirstLetter(item.name)}</CardTitle>
-                                      <CardDescription>
-                                        {item.email}
-                                      </CardDescription>
-                                    </div>
-                                  </CardHeader>
-                                  <CardContent>
-                                    <p className="select-none">{item.body}</p>
-                                  </CardContent>
-                                </Card>
-                              );
-                            })}
-                          </>
-                        );
-                      }}
-                    </DataFetcher>
-                  </ScrollArea>
-                </ResizablePanel>
-              </ResizablePanelGroup>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        );
-      }}
-    </DataFetcher>
+    <main className="flex h-dvh items-center justify-center w-full py-5">
+      <DataFetcher url="/posts">
+        {(data) => {
+          return (
+            <ResizablePanelGroup
+              direction="horizontal"
+              className="max-w-md rounded-lg border md:min-w-7xl"
+            >
+              <ResizablePanel defaultSize={50} className="">
+                <ScrollArea className="h-full">
+                  <div className="flex flex-col  py-6">
+                    {data?.map((item: any) => (
+                      <div
+                        key={item.id}
+                        className={cn(
+                          " p-2 line-clamp-1 cursor-pointer",
+                          item.id === id ? "bg-amber-700" : "hover:bg-red-500"
+                        )}
+                        onClick={() => setId(item.id)}
+                      >
+                        <span className="font-semibold">{item.title}</span>
+                      </div>
+                    ))}
+                  </div>
+                </ScrollArea>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={50}>
+                <ResizablePanelGroup direction="vertical">
+                  <ResizablePanel defaultSize={25}>
+                    <ScrollArea className="">
+                      <DataFetcher url={`/photos/${id}`}>
+                        {(data) => {
+                          return (
+                            <Image
+                              src={data?.url}
+                              loader={() => {
+                                return data?.thumbnailUrl;
+                              }}
+                              alt={data?.title}
+                              width={500}
+                              height={500}
+                            />
+                          );
+                        }}
+                      </DataFetcher>
+                    </ScrollArea>
+                  </ResizablePanel>
+                  <ResizableHandle />
+                  <ResizablePanel defaultSize={75}>
+                    <ScrollArea className="h-full px-4">
+                      <DataFetcher url={`/comments?postId=${id}`}>
+                        {(data) => {
+                          return (
+                            <>
+                              {data?.map(
+                                (
+                                  item: {
+                                    id:string,
+                                    name: string;
+                                    email: string;
+                                    body: string;
+                                  },
+                                  index: number
+                                ) => {
+                                  return (
+                                    <Card
+                                    key={item.id}
+                                      className={cn(
+                                        "drop-shadow-sm",
+                                        index === 0 || index === data.length - 1
+                                          ? "my-1"
+                                          : "my-5"
+                                      )}
+                                    >
+                                      <CardHeader className="flex flex-row items-start gap-3">
+                                        <Avatar className="size-12">
+                                          <AvatarFallback>
+                                            <User2Icon />
+                                          </AvatarFallback>
+                                        </Avatar>
+                                        <div className="flex flex-col">
+                                          <CardTitle>
+                                            {capitalizeFirstLetter(item.name)}
+                                          </CardTitle>
+                                          <CardDescription>
+                                            {item.email}
+                                          </CardDescription>
+                                        </div>
+                                      </CardHeader>
+                                      <CardContent>
+                                        <p className="select-none">
+                                          {item.body}
+                                        </p>
+                                      </CardContent>
+                                    </Card>
+                                  );
+                                }
+                              )}
+                            </>
+                          );
+                        }}
+                      </DataFetcher>
+                    </ScrollArea>
+                  </ResizablePanel>
+                </ResizablePanelGroup>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          );
+        }}
+      </DataFetcher>
+    </main>
   );
 }
